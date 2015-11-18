8fe8248867bef3ddbc1909942a2df8b2e3743e47
Michael Henriksen <micoman13@hotmail.com>
2015-11-18 13:49:47 +0100

    Models + get;set; controllers oprettet

---
+++ VejleSygehus/VejleSygehus/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VejleSygehus.Models {
+    public class Category {
+        public string Name { get; set; }
+    }
+}
---
+++ VejleSygehus/VejleSygehus/Models/IArticle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models; 
+
+namespace VejleSygehus.Models {
+    interface IArticle {
+        public string Path { get; set; }
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public Category Category { get; set; }
+    }
+}
