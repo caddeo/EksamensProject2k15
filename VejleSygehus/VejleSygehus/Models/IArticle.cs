d633aaeda8b818d66bcedf63c4c9bc6da808f9c3
Patrick Caddeo Nielsen <poss_en@hotmail.com>
2015-11-18 13:51:18 +0100

    interface fixxxxxxx

--- VejleSygehus/VejleSygehus/Models/IArticle.cs
+++ VejleSygehus/VejleSygehus/Models/IArticle.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Models; 
+using VejleSygehus.Models; 
 
 namespace VejleSygehus.Models {
     interface IArticle {
-        public string Path { get; set; }
-        public string Header { get; set; }
-        public string Body { get; set; }
-        public Category Category { get; set; }
+        string Path { get; set; }
+        string Header { get; set; }
+        string Body { get; set; }
+        Category Category { get; set; }
     }
 }
