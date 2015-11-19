24dc557ab51de577d470de8ebe4e711a08b4d59e
Patrick Caddeo Nielsen <poss_en@hotmail.com>
2015-11-19 06:07:00 +0100

    Jeg vågnede tidligt
    Så tænkte jeg ville lave mit eksempel færdigt
    
    Det er nu i hver sine metoder. Der er lidt mere funktion og "modulært" over det.
    
    Lavede også noget fejlhåndtering for at prøve.
    
    Jeg har ikke lavet delete-funktionen, men det skulle være ret nemt.
    
    Jeg har prøvet at kommentere det så meget som muligt. Jeg lavede det lidt så jeg selv kunne forstå hvordan man gjorde.

--- JsonExample/JsonExample/Program.cs
+++ JsonExample/JsonExample/Program.cs
@@ -14,48 +14,180 @@ namespace JsonExample
     {
         static void Main(string[] args)
         {
+            // Programmet kan lave en json fil af typen Produkt
+            // som indeholder hardcodede værdier
+            // Den kan gemme i en mappe som man bestemmer
+            // og så læse det op igen
+
+            // Muligvis lave try catch på metoderne
+            // så at hvis f.eks. filen ikke kan findes
+            // eller hvad der sker nogen skrive fejl
+
             Program program = new Program();
 
             // Opret et produkt som skal laves om til json fil
             Product product = new Product();
             product.Name = "Apple";
-            product.ExpiryDate = new DateTime(2008, 12, 28);
+            product.ExpiryDate = new DateTime(2009, 12, 28);
             product.Price = 3.99M;
             product.Sizes = new string[] { "Small", "Medium", "Large" };
-
-            // find rooten af filerne
-            string root = AppDomain.CurrentDomain.BaseDirectory;
 
             // giv det et navn
             Console.WriteLine("Gem en json fil");
             Console.WriteLine("Skriv fil navn (uden .json)");
-            string path = Console.ReadLine();
+            string filename = Console.ReadLine();
 
-            // gem filen med navnet
-            string json = JsonConvert.SerializeObject(product, Formatting.Indented);
-            File.WriteAllText(@root+"\\"+ path+".json", json);
+            // finder stien (og opretter den)
+            string path = program.GetPath(filename);
 
-            Product deserializedProduct;
+            // Serializer objektet ( af typen produkt ) og
+            // laver det om til en .json fil
+            // og gemmer det på den valgte sti
+            program.CreateJson(path, product);
 
-            // find filen med navnet
+            // Viser alle filerne i mappen 
+            // Kun lavet for at teste
+            Console.WriteLine("Alle json filer i mappen:");
+            // Ignorer - bare for at gøre så det mindre ligner et clusterfuck
+            Console.WriteLine(path.Substring(0, (path.Length - (filename.Length+".json".Length))));
+            Console.WriteLine(program.ListDirectory("json"));
+
+            // Få brugeren til at skrive et navn
             Console.WriteLine("Load en json fil");
             Console.WriteLine("Skriv fil navn (uden .json)");
-            path = Console.ReadLine();
+            filename = Console.ReadLine();
 
-            // deserialize JSON directly from a file
-            using (StreamReader file = File.OpenText(@root + "\\" + path + ".json"))
+            // finder stien
+            path = program.GetPath(filename);
+
+            // Finder filen og deserializer json om
+            // til typen Produkt
+            Product deserializedProduct = program.LoadJson(path);
+
+            // Udskriver objektet hvis det ikke er null
+            // Hvis den er null kunne den ikke findes
+            if(deserializedProduct != null)
             {
+                Console.WriteLine(string.Format("Produkt med {0}, {1}, {2}, {3}", deserializedProduct.Name,
+                deserializedProduct.ExpiryDate.ToString("yy-MM-dd"), deserializedProduct.Price, deserializedProduct.Sizes.Length));
+            }
+
+            Console.WriteLine("Tast på en knap for at lukke program");
+            Console.ReadKey();
+        }
+
+        public string GetPath(string filename)
+        {
+            // find rooten af filerne
+            string root = AppDomain.CurrentDomain.BaseDirectory;
+
+            // vi tager root folderen af projektet og den 
+            // mappe json filerne skal være i (kaldet json) 
+            string directorypath = @root + "\\json\\";
+
+            // tager inputtet og sørger for
+            // at det bliver til en .json fil
+            filename = filename + ".json";
+
+            // Vi opretter mappen "json"
+            Directory.CreateDirectory(directorypath);
+
+            // Vi sætter filnavnet sammen med hvor filen skal være
+            // hvor vi så får en sti hvor den skal oprettes
+            string path = Path.Combine(directorypath, filename);
+
+            // retunere stien filen skal være på
+            return path;
+        }
+
+        public void CreateJson(string path, Product product)
+        {
+            // gem filen med navnet på stien
+            string json = JsonConvert.SerializeObject(product, Formatting.Indented);
+            File.WriteAllText(path, json);
+        }
+
+        public Product LoadJson(string path)
+        {
+            // Laver et produkt, som er det produkt der kommer
+            // fra json filen som bliver deserialized
+            Product deserializedProduct;
+
+            // deserialize JSON directly from a file from path
+            try
+            {
+                using (StreamReader file = File.OpenText(path))
+                {
                     JsonSerializer serializer = new JsonSerializer();
                     deserializedProduct = (Product)serializer.Deserialize(file, typeof(Product));
                 }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Kunne ikke finde fil");
+                deserializedProduct = null;
+            }
 
-            Console.WriteLine(string.Format("Produkt med {0}, {1}, {2}, {3}", deserializedProduct.Name,
-                deserializedProduct.ExpiryDate.ToString("yy-MM-dd"), deserializedProduct.Price, deserializedProduct.Sizes.Length));
+            return deserializedProduct;
+        }
 
-            Console.ReadKey();
+        public void DeleteJson(string path, string filename)
+        {
+
+        }
+
+        public string ListDirectory(string mappath)
+        {
+            // Finder en fil ud fra stien
+            // På trods af eksemplet er meget simpelt
+            // (mappen vil altid hedde json) så laver 
+            // jeg det lidt mere kompliceret
+
+            // copy paste fra de andre metoder
+            string root = AppDomain.CurrentDomain.BaseDirectory;
+            string directorypath = @root + "\\"+mappath+"\\";
+
+
+            string[] filepaths = null;
+
+            // Finder alle filerne på stien som en liste med string
+            // og sortere kun de filer der er af typen .json
+            try
+            {
+                filepaths = Directory.GetFiles(directorypath, "*.json");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Kunne ikke finde mappe");
+            }
+
+            string filepath = "";
+
+            // filepaths == null hvis ikke den kan finde mappen
+            if (filepaths != null)
+            if( filepaths.Length > 0 )
+            {
+                // Fordi vi kun vil have en string
+                // så looper vi igennem alle filerne
+                // og tilføjer dem til vores return string
+                foreach(string file in filepaths)
+                {
+                    // gør så at root folderen ikke bliver vist men kun filnavnet
+                    string raw = file.Substring(directorypath.Length, (file.Length - directorypath.Length));
+                    filepath += raw + "\n";
+                }
+                // fjern mellemrummet til sidst...
+                filepath = filepath.Substring(0, (filepath.Length - "\n".Length));
+            }
+            else
+            {
+                filepath = "...ingen json filer";
+            }
+            return filepath;
         }
     }
 
+    // Produkt klassen som vi de/serializere med 
     public class Product
     {
         public string Name { get; set; }
