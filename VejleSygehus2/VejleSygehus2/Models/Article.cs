baedb76ecd60e4dcb068f0b0e85d7db4d1231165
Patrick Caddeo Nielsen <poss_en@hotmail.com>
2015-11-20 09:01:45 +0100

    + Article + canEdit i List.cshtml + Rerouting i List.cshtml

---
+++ VejleSygehus2/VejleSygehus2/Models/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VejleSygehus2.Models
+{
+    public class Article : IArticle
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class ArticleContext : DbContext
+    {
+        public ArticleContext() : base("ArticleContext") { }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
