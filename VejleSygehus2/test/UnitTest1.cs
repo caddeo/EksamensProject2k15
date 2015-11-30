0339d9be9a1aa08f484700e32e28e0d52ba9b815
Stefan Grundsøe Petersen <stef694e@edu.eal.dk>
2015-11-30 11:00:50 +0100

    fuld af lortekode

---
+++ VejleSygehus2/test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VejleSygehus2;
+using System.IO;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TID001()
+        {
+            VejleSygehus2.Models.Article article = new VejleSygehus2.Models.Article();
+            string path = "C:/Users/skole/Desktop/testmappe/Json/TID001.json";
+            article.Path = path;
+            article.Body = "Test body for TID001";
+            article.Header = "test header for TID001";
+            string json = JsonConvert.SerializeObject(article, Formatting.Indented);
+            File.WriteAllText(path, json);
+            VejleSygehus2.Service.JsonService jason = new VejleSygehus2.Service.JsonService();
+            VejleSygehus2.Models.Article result = jason.LoadJson(path);
+
+            Assert.AreEqual(article, result);
+        }
+    }
+}
