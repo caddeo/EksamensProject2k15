0e8c08d4fef45eabf4dbaf90ea18a513c4c90c69
Stefan Grundsøe Petersen <stef694e@edu.eal.dk>
2015-12-01 13:45:08 +0100

    saas

--- VejleSygehus2/VejleSygehusAPI2/Controllers/ArticleController.cs
+++ VejleSygehus2/VejleSygehusAPI2/Controllers/ArticleController.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using VejleSygehusAPI2.Models;
+using VejleSygehusAPI2.Service;
 
 namespace VejleSygehusAPI2.Controllers
 {
     public class ArticleController : ApiController
     {
-        
+        [HttpGet]
+        public Article Get(string path)
+        {
+            JsonService jservice = new JsonService();
+            Article article =  jservice.LoadJson(path);
+
+
+
+            return article;
+        }
     }
 }
---
+++ VejleSygehus2/VejleSygehusAPI2/Models/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VejleSygehusAPI2.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Models.Category Category { get; set; }
+    }
+}
